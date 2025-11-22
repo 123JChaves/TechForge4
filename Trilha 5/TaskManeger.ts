15f7ba3cb49fba5de9cb354c0f74288b2422f4b9
123JChaves <j.blues@hotmail.com>
2025-11-22 02:35:13 -0300

    Desafio trilha 5

---
+++ Trilha 5/TaskManeger.ts
@@ -0,0 +1,56 @@
+abstract class TaskMenager{
+    
+    abstract addTask(task: string): void;
+
+    abstract listTask(): string[];
+
+}
+
+class Project extends TaskMenager{
+    private task: string[];
+
+    constructor(task: string[]) {
+        super();
+        this.task = [];
+    }
+    addTask(task: string): void {
+        this.task.push(task);
+    }
+
+    listTask() {
+        return this.task;
+    }
+}
+
+class DailyTask extends TaskMenager{
+    private task: string[];
+
+    constructor(task: string[]) {
+        super();
+        this.task = [];
+    }
+
+    addTask(task: string): void {
+        this.task.push(task);
+    }
+
+    listTask() {
+        return this.task;
+    }
+
+}
+
+const project = new Project([]);
+
+project.addTask('Criar viajar');
+project.addTask('Criar Site');
+project.addTask('Criar Planner de Rotas');
+
+const dailyTask = new DailyTask([]);
+
+dailyTask.addTask('Ligar para o João');
+dailyTask.addTask('Ligar para o Maria');
+dailyTask.addTask('Estudar programação')
+
+console.log(project.listTask());
+console.log(dailyTask.listTask());
